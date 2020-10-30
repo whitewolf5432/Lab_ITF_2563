69bf039d03ffa719bf032359370e3dfadc809b72
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2020-10-30 13:00:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <img src="cat1.jpg"><br>
         <br>
         <a href="http://www.it.kmitl.ac.th">Link to IT KMITL Website</a>
+        <br>
         <a href="https://github.com/whitewolf5432/Lab_ITF_2563"><img src="cat.jpg"></a>
         <p>สวัสดีครับผมชื่อนายณัฐพัชร์ บัวคำโคตร์ ชื่อเล่นโปรแกรม หรือที่หลายๆคนเรียกว่า แกรม
             วันนี้ผมก็จะเล่าเรื่องตลกที่ตลกที่สุดในชีวิตของผมตั้งแต่ผมเกิดมาจากท้องแม่
